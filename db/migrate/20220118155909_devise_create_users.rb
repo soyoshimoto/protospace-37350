cf775a5feb5d434d436b8b3fa6ab99e48308a99e
soyoshimoto <96470876+soyoshimoto@users.noreply.github.com>
2022-01-30 15:11:07 +0900

    Update 20220118155909_devise_create_users.rb

--- db/migrate/20220118155909_devise_create_users.rb
+++ db/migrate/20220118155909_devise_create_users.rb
@@ -4,12 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false, default: ""
+      t.string :name,               null: false
       t.text :profile,              null: false
       t.text :occupation,           null: false
       t.text :position,             null: false
-      t.string :email,              null: false
-      t.string :encrypted_password, null: false
+      t.string :email,              null: false, default:""
+      t.string :encrypted_password, null: false, default:""
 
       ## Recoverable
       t.string   :reset_password_token
